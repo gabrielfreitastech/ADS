f48d48d6d58318ff54810f5fed311dbaef2038bc
GABRIEL HENRIQUE DE FREITAS <gabriel_freitas152@estudante.sesisenai.org.br>
2026-03-09 21:40:16 -0300

    Atividades de input

--- aula2arrays.js
+++ aula2arrays.js
@@ -123,6 +123,6 @@ console.log("______________________________");
 // b) Use flat para achatar o array em um unico nível (os arrays internos se tornam elementos individuais) e exiba-o.
 
 // → Seu código aqui:
-
+numeros2 = [[2,4,6],[1,3,5],[1,2,3,4,5]]
 
 console.log("_______________________________");
