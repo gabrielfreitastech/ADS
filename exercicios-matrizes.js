7205f8bcb1a902eb47833eaf4a08abf8bdd08c8d
Gabriel Freitas <gabriel.freitas02255@gmail.com>
2026-05-28 16:15:54 -0300

    Verificacao de pares

--- exercicios-matrizes.js
+++ exercicios-matrizes.js
@@ -1,14 +1,36 @@
 //Soma de todos os valores
+// const m = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9]
+// ]
+// let soma = 0
+
+// for(let i = 0; i < m.length; i++){
+//     for(let j = 0; j < m[i].length; j++){
+//         soma += m[i][j]
+//     }
+// }
+// console.log(`Soma total: ${soma}`)
+
+//Verificação de quantos numeros pares possui
 const m = [
   [1, 2, 3],
   [4, 5, 6],
   [7, 8, 9]
 ]
-let soma = 0
+
+let par = 0
 
 for(let i = 0; i < m.length; i++){
-    for(let j = 0; j < m[i].length; j++){
-        soma += m[i][j]
+  for(let j = 0; j < m[i].length; j++){
+
+    if(m[i][j] % 2 === 0){
+      console.log(`
+        Numeros pares da matriz:
+        m[${i}][${j}] = ${m[i][j]}`)
+        par++
     }
+  }
 }
-console.log(`Soma total: ${soma}`)
+console.log(`\nTotal de numeros pares: ${par}`)
