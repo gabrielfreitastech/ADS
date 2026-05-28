27e4deb947980d38703d41f356e8da077bb0faef
Gabriel Freitas <gabriel.freitas02255@gmail.com>
2026-05-28 19:10:06 -0300

    Encontrando valores

--- exercicios-matrizes.js
+++ exercicios-matrizes.js
@@ -1,3 +1,7 @@
+const rl = require("readline-sync")
+
+//Exercício 1 — Soma total da matriz
+
 //Soma de todos os valores
 // const m = [
 //   [1, 2, 3],
@@ -12,6 +16,8 @@
 //     }
 // }
 // console.log(`Soma total: ${soma}`)
+
+//Exercício 2 — Contar números pares
 
 //Verificação de quantos numeros pares possui
 // const m = [
@@ -35,16 +41,45 @@
 // }
 // console.log(`\nTotal de numeros pares: ${par}`)
 
+//Exercício 3 — Multiplicar todos os valores por 2
+
+// const m = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9]
+// ]
+
+// for(let i = 0; i < m.length; i++){
+//   for(let j = 0; j < m[i].length; j++){
+//     m[i][j] *= 2
+    
+//   }
+// }
+// console.table(m)
+
+//Exercício 4 — Encontrar a posição de um número
+
 const m = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9]
+  [12, 7, 25],
+  [3, 18, 9],
+  [31, 14, 22]
 ]
+
+console.table(m)
+let procurado = rl.questionInt('Digite um numero para encontrar a posicao: ')
+let achou = false
 
 for(let i = 0; i < m.length; i++){
   for(let j = 0; j < m[i].length; j++){
-    m[i][j] *= 2
-    
+    if(m[i][j] === procurado){
+      console.log(`Numero encontrado: m[${i}][${j}] = ${procurado}`)
+      achou = true
+      break
+    }
   }
+  if(achou){
+    break
+  }
+} if(!achou){
+  console.log('Number not found!')
 }
-console.table(m)
